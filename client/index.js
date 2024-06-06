fdbfd040b6c2514cc95dd631f75af142a3928d93
Marcelo <marcelorubenayala@outlook.com>
2024-06-05 23:15:11 -0300

    Deploy stuff :rocket:

--- client/index.js
+++ client/index.js
@@ -1,150 +1,147 @@
-const pokemonAPI = {};
+// Add an event listener to ensure the DOM is fully loaded before running the script.
 
-fetch("/pokeAPI")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((pokemon) => {
-      pokemonAPI[pokemon.id] = pokemon;
-    });
+document.addEventListener("DOMContentLoaded", function() {
+  const pokemonAPI = {};
 
-    const pokemonIds = [
-      "1", "1", "2", "2", "3", "3", "4", "4",
-      "5", "5", "6", "6", "7", "7", "9", "9"
-    ];
+  fetch("/pokeAPI")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((pokemon) => {
+        pokemonAPI[pokemon.id] = pokemon;
+      });
 
+      const pokemonIds = [
+        "1", "1", "2", "2", "3", "3", "4", "4",
+        "5", "5", "6", "6", "7", "7", "9", "9"
+      ];
 
-    let shuffledPokemonIds = pokemonIds.sort(() => Math.random() - 0.5);
+      let shuffledPokemonIds = pokemonIds.sort(() => Math.random() - 0.5);
 
-    let flippedCards = [];
-    let matchedPairs = 0;
+      let flippedCards = [];
+      let matchedPairs = 0;
 
+      let seconds = 0;
+      let minutes = 0;
+      let finalTime = '';
+      let movesCount = 0;
+      let timerInterval;
 
-    let seconds = 0;
-    let minutes = 0;
-    let finalTime = '';
-    let movesCount = 0;
-    let timerInterval;
+      let gameStarted = false;
 
-
-    let gameStarted = false;
-
-    function stopTimer() {
-      clearInterval(timerInterval);
-      gameStarted = false;
-    }
-
-    function startTimer() {
-      timerInterval = setInterval(updateTimer, 1000);
-      gameStarted = true;
-    }
-
-    function updateTimer() {
-      seconds += 1;
-      if (seconds >= 60) {
-        minutes += 1;
-        seconds = 0;
+      function stopTimer() {
+        clearInterval(timerInterval);
+        gameStarted = false;
       }
 
-      let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
-      let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
-
-
-      document.getElementById(
-        "time"
-      ).innerHTML = `<span>Time: ${minutesValue}:${secondsValue}</span>`;
-      finalTime = `${minutesValue}:${secondsValue}`
-    }
-
-    function updateMoves() {
-      movesCount++;
-
-      document.getElementById("moves").innerHTML = `<span>Moves: ${movesCount}</span>`;
-    }
-
-
-    function initializeTimer() {
-      if (!gameStarted) {
+      function startTimer() {
         timerInterval = setInterval(updateTimer, 1000);
         gameStarted = true;
       }
-    }
 
-
-    function createPokemonBox(pokemonId) {
-      const data = pokemonAPI[pokemonId];
-      const box = document.createElement("div");
-      box.className = "item";
-      box.dataset.id = pokemonId;
-      const img = document.createElement("img");
-      img.src = data.sprites.front_default;
-      img.alt = data.name;
-      box.appendChild(img);
-
-    
-      box.onclick = function () {
-        initializeTimer();
-
-        if (
-          flippedCards.length < 2 &&
-          !flippedCards.includes(this) &&
-          !this.classList.contains("boxOpen")
-        ) {
-          this.classList.add("boxOpen");
-          flippedCards.push(this);
-
-          if (flippedCards.length === 2) {
-            const card1 = flippedCards[0].getAttribute("data-id");
-            const card2 = flippedCards[1].getAttribute("data-id");
-
-            if (card1 === card2) {
-              flippedCards = [];
-              matchedPairs++;
-
-              if (matchedPairs === shuffledPokemonIds.length / 2) {
-                setTimeout(() => {
-                  let user = prompt("Ranking");
-                  let time = finalTime;
-                  let moves = movesCount;
-                  fetch('/api/winner', {
-                    method: 'POST',
-                    headers: {
-                      "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify({user, moves, time})
-                  })
-                  .then((response) => response.json())
-                  .then((data) => console.log(data))
-                  .catch((err) => {
-                    console.log(`Error ${err}`)
-                  });
-                  stopTimer();
-                }, 500);
-              }
-            } else {
-              setTimeout(() => {
-                flippedCards.forEach((card) =>
-                  card.classList.remove("boxOpen")
-                );
-                flippedCards = [];
-              }, 1000);
-            }
-
-            updateMoves();
-          }
+      function updateTimer() {
+        seconds += 1;
+        if (seconds >= 60) {
+          minutes += 1;
+          seconds = 0;
         }
-      };
 
-      document.querySelector(".game").appendChild(box);
-    }
+        let secondsValue = seconds < 10 ? `0${seconds}` : seconds;
+        let minutesValue = minutes < 10 ? `0${minutes}` : minutes;
 
-    async function createGame() {
-      for (let i = 0; i < shuffledPokemonIds.length; i++) {
-        createPokemonBox(shuffledPokemonIds[i]);
+        document.getElementById(
+          "time"
+        ).innerHTML = `<span>Time: ${minutesValue}:${secondsValue}</span>`;
+        finalTime = `${minutesValue}:${secondsValue}`
       }
-    }
 
-    createGame();
-  })
-  .catch((err) => {
-    console.error(`Error getting Pokémon data ${err}`);
-  });
+      function updateMoves() {
+        movesCount++;
+
+        document.getElementById("moves").innerHTML = `<span>Moves: ${movesCount}</span>`;
+      }
+
+      function initializeTimer() {
+        if (!gameStarted) {
+          timerInterval = setInterval(updateTimer, 1000);
+          gameStarted = true;
+        }
+      }
+
+      function createPokemonBox(pokemonId) {
+        const data = pokemonAPI[pokemonId];
+        const box = document.createElement("div");
+        box.className = "item";
+        box.dataset.id = pokemonId;
+        const img = document.createElement("img");
+        img.src = data.sprites.front_default;
+        img.alt = data.name;
+        box.appendChild(img);
+
+        box.onclick = function () {
+          initializeTimer();
+
+          if (
+            flippedCards.length < 2 &&
+            !flippedCards.includes(this) &&
+            !this.classList.contains("boxOpen")
+          ) {
+            this.classList.add("boxOpen");
+            flippedCards.push(this);
+
+            if (flippedCards.length === 2) {
+              const card1 = flippedCards[0].getAttribute("data-id");
+              const card2 = flippedCards[1].getAttribute("data-id");
+
+              if (card1 === card2) {
+                flippedCards = [];
+                matchedPairs++;
+
+                if (matchedPairs === shuffledPokemonIds.length / 2) {
+                  setTimeout(() => {
+                    let user = prompt("Ranking");
+                    let time = finalTime;
+                    let moves = movesCount;
+                    fetch('/api/winner', {
+                      method: 'POST',
+                      headers: {
+                        "Content-Type": "application/json",
+                      },
+                      body: JSON.stringify({user, moves, time})
+                    })
+                    .then((response) => response.json())
+                    .then((data) => console.log(data))
+                    .catch((err) => {
+                      console.log(`Error ${err}`)
+                    });
+                    stopTimer();
+                  }, 500);
+                }
+              } else {
+                setTimeout(() => {
+                  flippedCards.forEach((card) =>
+                    card.classList.remove("boxOpen")
+                  );
+                  flippedCards = [];
+                }, 1000);
+              }
+
+              updateMoves();
+            }
+          }
+        };
+
+        document.querySelector(".game").appendChild(box);
+      }
+
+      async function createGame() {
+        for (let i = 0; i < shuffledPokemonIds.length; i++) {
+          createPokemonBox(shuffledPokemonIds[i]);
+        }
+      }
+
+      createGame();
+    })
+    .catch((err) => {
+      console.error(`Error getting Pokémon data ${err}`);
+    });
+});
